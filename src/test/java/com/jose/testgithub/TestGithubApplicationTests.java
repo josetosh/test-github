08733d8d9ea853763264435e2a7313610e248620
U-HERCULES\ken16849 <ken16849@ITR5CD201BLD5.kcbad.com>
2023-10-05 15:26:52 +0300

    Sending Project to GitHub First Time

---
+++ src/test/java/com/jose/testgithub/TestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.testgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
